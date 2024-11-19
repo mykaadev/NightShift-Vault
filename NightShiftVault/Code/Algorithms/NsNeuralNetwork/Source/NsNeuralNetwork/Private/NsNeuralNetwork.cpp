1d9074877f6fd218a1dca1279b10a8105d218b17
mykaa <101289701+mykaadev@users.noreply.github.com>
2024-11-19 17:19:32 +0000

    Init

---
+++ NightShiftVault/Code/Algorithms/NsNeuralNetwork/Source/NsNeuralNetwork/Private/NsNeuralNetwork.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NsNeuralNetwork.h"
+
+#define LOCTEXT_NAMESPACE "FNsNeuralNetworkModule"
+
+void FNsNeuralNetworkModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FNsNeuralNetworkModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FNsNeuralNetworkModule, NsNeuralNetwork)
