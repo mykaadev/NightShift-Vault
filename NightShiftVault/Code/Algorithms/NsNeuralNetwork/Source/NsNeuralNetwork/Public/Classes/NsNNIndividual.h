dfc3df536311f16f281453931fc4d7eafa4f8bd8
mykaa <micaelsilva.work@gmail.com>
2024-12-09 16:37:29 +0000

    Added Neural Framework
    - Train Sessions
    - Export Session Data

---
+++ NightShiftVault/Code/Algorithms/NsNeuralNetwork/Source/NsNeuralNetwork/Public/Classes/NsNNIndividual.h
@@ -0,0 +1,85 @@
+﻿// Copyright © 2024 mykaa. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "NsNNIndividual.generated.h"
+
+/**
+ * Neural Network Individual
+ */
+UCLASS()
+class NSNEURALNETWORK_API UNsNNIndividual : public UObject
+{
+    GENERATED_BODY()
+
+// Functions
+public:
+
+    /** Constructor */
+    UNsNNIndividual();
+
+    /** Construct */
+    UFUNCTION()
+    void Construct(const FRandomStream InRandomStream, const int32 InSize);
+
+    /** Construct Individual with a predefined genotype */
+    void Construct(const FRandomStream InRandomStream, const int32 InSize, const TArray<float>& InGenotype);
+
+    /** Get Fitness */
+    UFUNCTION()
+    float GetFitness() const;
+
+    /** Set Fitness */
+    UFUNCTION()
+    void SetFitness(const float InFitness);
+
+    /** Get Size */
+    UFUNCTION()
+    int32 GetSize() const;
+
+    /** Set Size */
+    UFUNCTION()
+    void SetSize(int32 InSize);
+
+    /** Get Genotype */
+    UFUNCTION()
+    TArray<float> GetGenotype() const;
+
+    /** Get mutable genotype */
+    UFUNCTION()
+    TArray<float>& GetMutableGenotype();
+
+    /** Set Genotype */
+    UFUNCTION()
+    void SetGenotype(const TArray<float>& InSize);
+
+    /** Copy from Individual */
+    UFUNCTION()
+    void CopyFromIndividual(const UNsNNIndividual* const InIndividual);
+
+    /** Recombine */
+    UFUNCTION()
+    void Recombine(const UNsNNIndividual* const InIndividual, float InProbability, FRandomStream InRandomStream);
+
+    /** Mutation */
+    UFUNCTION()
+    void Mutation(const float InProbability, const FRandomStream InStream);
+
+
+// Variables
+public:
+
+    /** Genotype */
+    UPROPERTY()
+    TArray<float> Genotype;
+
+    /** Size */
+    UPROPERTY()
+    int32 Size;
+
+    /** Fitness */
+    UPROPERTY()
+    float Fitness;
+};
---
+++ NightShiftVault/Code/Algorithms/NsNeuralNetwork/Source/NsNeuralNetwork/Public/UI/NsNNControlPanel.h
@@ -0,0 +1,66 @@
+﻿// Copyright © 2024 mykaa. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NsNNControlPanel.generated.h"
+
+/**
+ * Neural Network Control Panel
+ */
+UCLASS()
+class NSNEURALNETWORK_API UNsNNControlPanel : public UUserWidget
+{
+    GENERATED_BODY()
+
+// Functions
+public:
+
+    //~ Begin UUserWidget Interface
+    virtual void NativePreConstruct() override;
+    //~ End UUserWidget Interface
+
+    /** Starts the train session */
+    UFUNCTION(BlueprintCallable)
+    void RequestStartTrainSession() const;
+
+    /** Stops the train session */
+    UFUNCTION(BlueprintCallable)
+    void RequestStopTrainSession() const;
+
+// Variables
+public:
+
+    /** SpinBox to define Random Seed */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBRandomSeed;
+
+    /** SpinBox to define Population Size */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBPopulationSize;
+
+    /** SpinBox to define Individual Time */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBMaxIndividualTime;
+
+    /** Slider to define Recombination Probability */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBRecombination;
+
+    /** Slider to define Mutation Probability */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBMutation;
+
+    /** SpinBox to define Random Seed */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBInputs;
+
+    /** SpinBox to define Population Size */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBHiddenLayer;
+
+    /** SpinBox to define Individual Time */
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(BindWidget))
+    class USpinBox* SBOutput;
+};
